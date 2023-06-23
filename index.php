2a7b1799b4e9531ffbd57bacc4ff1048be196718
lqasemzadeh <lqasemzadeh@gmail.com>
2023-06-23 12:03:11 +0330

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>NationalCode</title>
+</head>
+<body>
+<form action="check.php" method="post">
+    National Code<input type="text" name="national_code" id="">
+    <input type="submit" value="check">
+</form>
+</body>
+</html>
